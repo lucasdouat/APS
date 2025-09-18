9ca8b1185539e4e89285d5e5f9ba59588aa2ac90
lucasdouat <ldouat@estudiantes.unsam.edu.ar>
2025-09-17 21:02:29 -0300

    Tarea Semanal N°3

--- ts3.py
+++ ts3.py
@@ -45,7 +45,7 @@ plt.figure(1)
 plt.clf()
 plt.plot(ll,np.abs(X1),'x',label='|X1(f)|')
 plt.plot(ll,np.abs(X2),'o',label='|X2(f)|')
-plt.plot(ll,np.abs(X3),'*',label='X3(f)|')
+plt.plot(ll,np.abs(X3),'*',label='|X3(f)|')
 plt.title('Espectro en Magnitud')
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('|X[k]|')
@@ -72,13 +72,56 @@ plt.xlim([0, fs/2])
 plt.show()
 
 
-# Parseval
-energy_time = np.sum(x1**2)
-energy_freq = np.sum(np.abs(X1)**2)/N
-print("Energía temporal x1:", energy_time)
-print("Energía frecuencial X1:", energy_freq)
 
-energy_time = np.sum(x2**2)
-energy_freq = np.sum(np.abs(X2)**2)/N
-print("Energía temporal x2:", energy_time)
-print("Energía frecuencial X2:", energy_freq)
+#%% d) Parseval
+
+# Normalización para potencia unitaria (varianza = 1)
+x1_norm = x1 / np.sqrt(np.var(x1))
+x2_norm = x2 / np.sqrt(np.var(x2))
+x3_norm = x3 / np.sqrt(np.var(x3))
+
+# FFT de las señales normalizadas
+X1 = fft.fft(x1_norm)
+X2 = fft.fft(x2_norm)
+X3 = fft.fft(x3_norm)
+
+# Verificación de Parseval
+energy_time_x1 = np.sum(x1_norm**2)
+energy_freq_x1 = np.sum(np.abs(X1)**2) / N
+
+energy_time_x2 = np.sum(x2_norm**2)
+energy_freq_x2 = np.sum(np.abs(X2)**2) / N
+
+energy_time_x3 = np.sum(x3_norm**2)
+energy_freq_x3 = np.sum(np.abs(X3)**2) / N
+
+# Resultados
+print("x1 - Varianza:", np.var(x1_norm), "Energía temporal:", energy_time_x1, "Energía frecuencial:", energy_freq_x1)
+print("x2 - Varianza:", np.var(x2_norm), "Energía temporal:", energy_time_x2, "Energía frecuencial:", energy_freq_x2)
+print("x3 - Varianza:", np.var(x3_norm), "Energía temporal:", energy_time_x3, "Energía frecuencial:", energy_freq_x3)
+
+#%% c) Aplicar Zero Padding
+x1_zp = np.concatenate([x1, np.zeros(9*N)])
+X1_zp = fft.fft(x1_zp)
+x2_zp = np.concatenate([x2, np.zeros(9*N)])
+X2_zp = fft.fft(x2_zp)
+x3_zp = np.concatenate([x3, np.zeros(9*N)])
+X3_zp = fft.fft(x3_zp)
+
+#Nuevo Vector de Frecuencia
+ll_zp = np.linspace(0, fs, len(X1_zp)).flatten()
+
+# Graficas de Espectro en dB con Zeo Padding
+plt.figure(3)
+plt.clf()
+plt.plot(ll_zp, 20*np.log10(np.abs(X1_zp)),'x',label='X1 dB')
+plt.plot(ll_zp, 20*np.log10(np.abs(X2_zp)),'o',label='X2 dB')
+plt.plot(ll_zp, 20*np.log10(np.abs(X3_zp)),'*',label='X3 dB')
+plt.title("FFT con Zero Padding")
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Magnitud (dB)")
+plt.grid(True)
+plt.tight_layout()
+plt.legend()
+plt.xlim([220, 280])
+plt.show()
